733a13789c89f551625662fc58d64c114b0907f7
Lazaro <lazllb@gmail.com>
2013-01-23 20:52:39 -0500

    Added an AbstractWrapper class. Also implemented game objects and their definitions. Wrote a test script.

--- src/rs2/environment/RS2Environment.java
+++ src/rs2/environment/RS2Environment.java
@@ -23,7 +23,7 @@ import rs2.environment.wrapper.entity.Player;
 
 /**
  * @author Lazaro Brito.
- *
+ * 
  */
 public class RS2Environment {
 	/**
@@ -31,48 +31,52 @@ public class RS2Environment {
 	 * 
 	 * Checks if the model is null to prevent null pointer exceptions.
 	 * 
-	 * @param model The model to wrap.
-	 * @return The wrapper. 
+	 * @param model
+	 *            The model to wrap.
+	 * @return The wrapper.
 	 */
 	public static <W extends Wrapper<?>> W wrap(Model<W> model) {
-		if(model != null) {
+		if (model != null) {
 			return model.get_wrapper();
 		} else {
 			return null;
 		}
 	}
-	
+
 	/**
 	 * Queues the event into it's context entity.
-	 * @param event The event the queue.
+	 * 
+	 * @param event
+	 *            The event the queue.
 	 */
 	public static void queue(Event<?> event) {
-		if(event != null) {
+		if (event != null) {
 			event.context.queueEvent(event);
 		}
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		if(args[0].equals("imports")) {
+		if (args[0].equals("imports")) {
 			File dir = new File(args[1]);
-			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(args[2])));
+			BufferedWriter writer = new BufferedWriter(new FileWriter(new File(
+					args[2])));
 			ScriptBase.generateImports(dir, dir, writer);
 			writer.close();
 		} else {
 			System.err.println("Invalid argument: " + args[0]);
 		}
 	}
-	
+
 	private final World world;
-	
+
 	private final ScriptBase scripts;
 	private final Map<Integer, List<ObjectScript>> objectScripts = new HashMap<Integer, List<ObjectScript>>();
-	
+
 	public RS2Environment(World world) {
 		this.world = world;
-		
+
 		scripts = new ScriptBase(new String[] { "scripts/" });
-		
+
 		try {
 			loadScripts();
 		} catch (IOException e) {
@@ -80,72 +84,78 @@ public class RS2Environment {
 			e.printStackTrace();
 		}
 	}
-	
+
 	private void loadScripts() throws IOException {
-        BufferedReader cfg = new BufferedReader(new FileReader("data/script_list.txt"));
-        try {
-	        int lineNumber = 0;
-	        String line;
-	        while ((line = cfg.readLine()) != null) {
-	            if (line.startsWith("#") || line.trim().length() == 0) {
-	                continue;
-	            }
-	
-	            Script script = null;
-	            script = scripts.initScript(line.trim());
-	            
-	            if(script == null) {
-	            	System.err.println("Failed to load script (line #" + lineNumber + ") : " + line);            	
-	            } else {
-		            try {
-		            	script.initialize(this);
-		            } catch (Exception e) {
-		                System.err.println("Failed to initiate script (line #" + lineNumber + ") : " + line);
-		                e.printStackTrace();
-		            }
-	            }
-	
-	            lineNumber++;
-	        }
-        } finally {
-        	if(cfg != null) {
-        		cfg.close();
-        	}
-        }
+		BufferedReader cfg = new BufferedReader(new FileReader(
+				"data/script_list.txt"));
+		try {
+			int lineNumber = 0;
+			String line;
+			while ((line = cfg.readLine()) != null) {
+				if (line.startsWith("#") || line.trim().length() == 0) {
+					continue;
+				}
+
+				Script script = null;
+				script = scripts.initScript(line.trim());
+
+				if (script == null) {
+					System.err.println("Failed to load script (line #"
+							+ lineNumber + ") : " + line);
+				} else {
+					try {
+						script.initialize(this);
+					} catch (Exception e) {
+						System.err.println("Failed to initiate script (line #"
+								+ lineNumber + ") : " + line);
+						e.printStackTrace();
+					}
+				}
+
+				lineNumber++;
+			}
+		} finally {
+			if (cfg != null) {
+				cfg.close();
+			}
+		}
 	}
 
 	public void registerObjectScript(int[] ids, ObjectScript script) {
-		for(int i : ids) {
+		for (int i : ids) {
 			List<ObjectScript> scripts = objectScripts.get(i);
-			if(scripts == null) {
+			if (scripts == null) {
 				objectScripts.put(i, (scripts = new ArrayList<ObjectScript>()));
 			}
 			scripts.add(script);
 		}
 	}
-	
+
 	public List<ObjectScript> getObjectScripts(int id) {
 		return objectScripts.get(id);
 	}
-	
-	/**-
-	 * Gets the world instance associated with this environment.
+
+	/**
+	 * - Gets the world instance associated with this environment.
+	 * 
 	 * @return The world.
 	 */
 	public World getWorld() {
 		return world;
 	}
-	
+
 	/**
 	 * Processes all the events in the world's context.
-	 * @param world The world context.
+	 * 
+	 * @param world
+	 *            The world context.
 	 */
 	public void handle() {
-		for(Player player : world.getPlayers()) {
+		for (Player player : world.getPlayers()) {
 			player.handleEvents();
 		}
-		
-		for(NPC npc : world.getNPCs()) {
+
+		for (NPC npc : world.getNPCs()) {
 			npc.handleEvents();
 		}
 	}
